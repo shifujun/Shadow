67561d6f16f76c95c51814ad425f7f259367fe06
shifujun <shifujun@foxmail.com>
2021-07-05 16:44:09 +0800

    createPackageContext return this可以修复问题

--- projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/flutter/TestFlutterActivity.java
+++ projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/flutter/TestFlutterActivity.java
@@ -1,7 +1,14 @@
 package com.tencent.shadow.sample.plugin.app.lib.usecases.flutter;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class TestFlutterActivity extends FlutterActivity {
+    @Override
+    public Context createPackageContext(String packageName, int flags) throws PackageManager.NameNotFoundException {
+        return this;
+    }
 
 }
