34cccb30172628f94093354e51f8f43a36e58e73
bokeyhuang <bokeyhuang@tencent.com>
2021-12-13 16:13:15 +0800

    插件尝试使用依赖注入

---
+++ projects/test/plugin/androidx-cases/test-plugin-androidx-cases/src/main/java/com/tencent/shadow/test/plugin/androidx_cases/lib/AppCompatTestActivity.kt
@@ -0,0 +1,31 @@
+package com.tencent.shadow.test.plugin.androidx_cases.lib
+
+import android.os.Bundle
+import android.view.LayoutInflater.Factory2
+import androidx.appcompat.app.AppCompatActivity
+import com.tencent.cloudgamepluginbaseactivity.SplashViewModel
+import org.koin.android.ext.android.get
+
+class AppCompatTestActivity : AppCompatActivity() {
+    val splashViewModel : SplashViewModel = get()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+      //  System.out.println("bokeyhuang watch. plugin model=$splashViewModel")
+        super.onCreate(savedInstanceState)
+        val factory2 = layoutInflater.factory2
+        val factory2Class: String
+        factory2Class = if (factory2 == null) {
+            "null"
+        } else {
+            factory2.javaClass.name
+        }
+        val viewGroup = UiUtil.setActivityContentView(this)
+        val item = UiUtil.makeItem(
+            this,
+            "factory2Class",
+            "factory2Class",
+            factory2Class
+        )
+        viewGroup.addView(item)
+    }
+}
