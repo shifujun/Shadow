37e0c113d45669f3e75fefdb065b44ee8f83eaa8
shifujun <shifujun@foxmail.com>
2021-12-14 11:28:12 +0800

    test: 打印splashViewModel

--- projects/test/plugin/androidx-cases/test-plugin-androidx-cases/src/main/java/com/tencent/shadow/test/plugin/androidx_cases/lib/AppCompatTestActivity.kt
+++ projects/test/plugin/androidx-cases/test-plugin-androidx-cases/src/main/java/com/tencent/shadow/test/plugin/androidx_cases/lib/AppCompatTestActivity.kt
@@ -10,21 +10,14 @@ class AppCompatTestActivity : AppCompatActivity(), ComponentCallbacks2 {
     val splashViewModel: SplashViewModel = get()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //  System.out.println("bokeyhuang watch. plugin model=$splashViewModel")
         super.onCreate(savedInstanceState)
-        val factory2 = layoutInflater.factory2
-        val factory2Class: String
-        factory2Class = if (factory2 == null) {
-            "null"
-        } else {
-            factory2.javaClass.name
-        }
+        System.out.println("bokeyhuang watch. plugin model=$splashViewModel")
         val viewGroup = UiUtil.setActivityContentView(this)
         val item = UiUtil.makeItem(
             this,
-            "factory2Class",
-            "factory2Class",
-            factory2Class
+            "splashViewModel",
+            "splashViewModel",
+            "bokeyhuang watch. plugin model=$splashViewModel"
         )
         viewGroup.addView(item)
     }
