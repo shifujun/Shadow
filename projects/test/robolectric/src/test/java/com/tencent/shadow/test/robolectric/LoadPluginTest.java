d013bab02347e0a1d7bc6f43ccdd4f2f8f9105e6
shifujun <shifujun@foxmail.com>
2022-11-24 17:31:42 +0800

    done: 配置好maven代理，robolectric可以触发Application启动了。

---
+++ projects/test/robolectric/src/test/java/com/tencent/shadow/test/robolectric/LoadPluginTest.java
@@ -0,0 +1,17 @@
+package com.tencent.shadow.test.robolectric;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class LoadPluginTest {
+    @Test
+    public void loadPlugin() {
+//        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+//            controller.setup();
+//
+//        }
+        System.out.println("xx");
+    }
+}
